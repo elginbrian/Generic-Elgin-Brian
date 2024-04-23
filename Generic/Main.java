d903c493d8df2800acb34ec9d0eb7ae05364fc2d
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2024-04-23 11:54:29 +0700

    Add files via upload

---
+++ Generic/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        GenericClass<String> gen = new GenericClass<String>("Budi");
+        System.out.println(gen.getGenericData()); 
+    }
+}
